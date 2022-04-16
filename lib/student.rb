d15427d2b2c97aeabac997ab49431102a5e59f20
Joe <hutcheson2001@yahoo.com>
2022-04-16 18:36:52 -0400

    commit tree 448f930ddfe52daf2b534280b681f470e4a38d77

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative "./user"
+
+class Student < User
+  attr_accessor :first_name, :last_name
+
+  def initialize(knowledge = [])
+    @knowledge = knowledge
+  end
+
+  def learn know_string
+    @knowledge << know_string
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
